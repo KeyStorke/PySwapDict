49b8fe6682e3f8017fc0667a64c98e3df65ac826
KeyStorke <00x00h@gmail.com>
2016-11-03 21:59:32 +0000

    code refactoring

--- SwapDict.py
+++ SwapDict.py
@@ -12,15 +12,15 @@ class ContextManager:
     """ Context manager for IO operations for dict-file
     """
 
-    def __init__(self, filename, delete_file, lock, semaphore):
-        self.filename = filename
-        # checking exists file and removing him
-        if os.path.isfile(self.filename):
+    def __init__(self, swap_filename, delete_file, lock, semaphore):
+        self.swap_filename = swap_filename
+        # checking exists swap_file and removing him
+        if os.path.isfile(self.swap_filename):
             if delete_file:
-                os.remove(self.filename)
+                os.remove(self.swap_filename)
             else:
                 raise SystemError
-        self.file_instance = shelve.open(self.filename)
+        self.file_instance = shelve.open(self.swap_filename)
         self.file_instance.close()
         # init mutex for multithreading
         self.semaphore = semaphore
@@ -30,7 +30,7 @@ class ContextManager:
     def __enter__(self):
         self.semaphore.acquire()
         self.lock.acquire()
-        self.file_instance = shelve.open(self.filename)
+        self.file_instance = shelve.open(self.swap_filename)
         return self.file_instance
 
     def __exit__(self, *args):
@@ -46,15 +46,15 @@ class SwapDict:
     1. safe multithreading and multiprocessing
     2. swap data into disk space
     Args:
-    1. filename - swap file name
-    2. delete_file - status of delete file if him exists
+    1. swap_filename - swap swap_file name
+    2. delete_file - status of delete swap_file if him exists
     3. lock - multiprocessing.Lock() for safe multiprocessing work
     3. semaphore - threading.Semaphore() for safe mulithreading work
     """
 
     def __init__(
         self,
-        filename='swap_dict',
+        swap_filename='swap_dict',
         delete_file=True,
         lock=Lock(),
         semaphore=Semaphore(),
@@ -67,7 +67,7 @@ class SwapDict:
 
         # for safe multiprocessing
 
-        self.cm = ContextManager(filename=filename,
+        self.cm = ContextManager(swap_filename=swap_filename,
                                  delete_file=delete_file,
                                  semaphore=self.semaphore,
                                  lock=self.lock)
@@ -79,7 +79,7 @@ class SwapDict:
         self.int_keys = self.manager.dict()
 
     def __setitem__(self, key, value):
-        with self.cm as file:
+        with self.cm as swap_file:
             if type(key) == int:
 
                 # calculate checksum
@@ -93,68 +93,70 @@ class SwapDict:
 
                 # add to dict-file
 
-                file[hash] = value
+                swap_file[hash] = value
             else:
-                file[key] = value
+                swap_file[key] = value
 
     def __getitem__(self, key):
         value = None
-        with self.cm as file:
+        with self.cm as swap_file:
             if type(key) == int:
                 hash = md5(str(key)).hexdigest()
                 if hash not in self.int_keys:
                     raise KeyError
-                value = file[hash]
+                value = swap_file[hash]
             else:
-                value = file[key]
+                value = swap_file[key]
         return value
 
     def __str__(self):
-        with self.cm as file:
-            value = str(file)
+        with self.cm as swap_file:
+            value = str(swap_file)
         return value
 
     def __len__(self):
-        with self.cm as file:
-            value = len(file)
+        with self.cm as swap_file:
+            value = len(swap_file)
         return value
 
     def __iter__(self):
         value = dict()
-        with self.cm as file:
-            keys = file.keys()
-            values = file.values()
+        with self.cm as swap_file:
+            keys = swap_file.keys()
+            values = swap_file.values()
             ret = map(lambda k, v: value.update({k: v}), keys,
                       values).__iter__()
         return ret
 
     def __missing__(self, key):
-        with self.cm as file:
-            value = file.__missing__(key)
+        with self.cm as swap_file:
+            value = swap_file.__missing__(key)
         return value
 
     def __delitem__(self, key):
-        with self.cm as file:
+        with self.cm as swap_file:
             if type(key) == int:
                 hash = md5(str(key)).hexdigest()
                 if hash not in self.int_keys:
                     raise KeyError
-                del self.int_keys[self.int_keys.index(hash)]
-                del file[hash]
+                del self.int_keys[hash]
+                del swap_file[hash]
             else:
-                del file[key]
+                del swap_file[key]
 
     def values(self):
+        """ Return the list of values """
         values = list()
-        with self.cm as file:
-            values = list(file.values())
+        with self.cm as swap_file:
+            values = list(swap_file.values())
         return values
 
     def keys(self):
+        """ Return the list of keys """
         pseudo_keys = list()
         keys = list()
-        with self.cm as file:
-            pseudo_keys = file.keys()
+        with self.cm as swap_file:
+            pseudo_keys = swap_file.keys()
             for key in pseudo_keys:
                 if key in self.int_keys.keys():
                     keys.append(self.int_keys[key])
